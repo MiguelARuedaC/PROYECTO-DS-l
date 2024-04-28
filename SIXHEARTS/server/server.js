75acc310a8fcbf06fb3bdd0496183ce64fe1bc92
CesarOsorioP <osoriocesar024@gmail.com>
2024-04-28 18:03:21 -0500

    Solución de un problema con git

--- SIXHEARTS/server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require ('express')
-const app = express()
-
-app.get("/api", (req,res) => {
-    res.json({"users":["usuarioUno", "usarioDos", "usuarioTres"] })
-}) 
-
-app.listen(5000,()=>{ console.log("El servidor ha sido inicializado en el puerto 5000")})
